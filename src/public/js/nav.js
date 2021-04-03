fb41e397c4bc7adcd07b4b6d2c740949904d7c18
Wojtic <62419039+Wojtic@users.noreply.github.com>
2021-04-03 13:27:12 +0200

    Finished nav

--- src/public/js/nav.js
+++ src/public/js/nav.js
@@ -2,8 +2,10 @@ $(document).ready(function () {
     $(".burger").click(() => {
         if ($("nav ul").hasClass("nav_active")) {
             $("nav ul").removeClass("nav_active");
+            $(".burger").removeClass("toggle");
         } else {
             $("nav ul").addClass("nav_active");
+            $(".burger").addClass("toggle");
         }
     });
 })
