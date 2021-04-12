74438a537c2b9edfa1e08ebcc92057993daeb441
Wojtic <62419039+Wojtic@users.noreply.github.com>
2021-04-12 18:40:13 +0200

    Added logout etc. styling

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -6,7 +6,6 @@ $(document).ready(async () => {
     response = await response.json();
 
     if (response.loggedIn) {
-        console.log("here")
-        $("main").append(`<div class="user"><i class="fa fa-user-circle fa-2x"></i><a href="/log_out">Odhlásit se</a></div>`);
+        $("main").append(`<div class="user"><i class="fa fa-user-circle fa-4x"></i><a href="/log_out">Odhlásit se</a></div>`);
     }
 });
