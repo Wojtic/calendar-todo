08289b2e47032d782438ee48d6992fdb4cf78f82
Wojtic <62419039+Wojtic@users.noreply.github.com>
2021-04-29 10:05:19 +0200

    Added some nav logic.

--- front-end/src/Navigation.js
+++ front-end/src/Navigation.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
 import "./styles/css/style.css"
 
-export default function Navigation() {
-    return (
-        <nav>
-            <img src="./icons/test.png" alt="<h3>Logo</h3>" height="90%" />
-            <ul>
-                <li><a href="index.html">Domů</a></li>
-                <li><a href="calendar.html">Kalendář</a></li>
-                <li><a href="login.html">Log in</a></li>
-            </ul>
-            <div class="burger">
-                <div id="burger1"></div>
-                <div id="burger2"></div>
-                <div id="burger3"></div>
-            </div>
-        </nav>
-    )
+export default class Navigation extends Component {
+    state = { ulShown: false }
+
+    render() {
+        return (
+            <nav>
+                <img src="./icons/test.png" alt="<h3>Logo</h3>" height="90%" />
+                <ul className={this.state.ulShown ? "nav_active" : null}>
+                    <li><a href="index.html">Domů</a></li>
+                    <li><a href="calendar.html">Kalendář</a></li>
+                    <li><a href="login.html">Log in</a></li>
+                </ul>
+                <div className={`burger ${this.state.ulShown ? "toggle" : null}`} onClick={() => {
+                    this.setState({ ulShown: !this.state.ulShown })
+                }}>
+                    <div id="burger1"></div>
+                    <div id="burger2"></div>
+                    <div id="burger3"></div>
+                </div>
+            </nav>
+        )
+    }
 }
