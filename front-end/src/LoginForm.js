7820f6fb056408fca7229b54f02d8e7a8804bd68
Wojtic <62419039+Wojtic@users.noreply.github.com>
2021-04-29 12:14:48 +0200

    Added basic functionality to login form

--- front-end/src/LoginForm.js
+++ front-end/src/LoginForm.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import './styles/css/style.css';
 
 export default class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { email: '', password: '', user_name: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
         return (
-            <form action="#" id="login_form">
+            <form action="#" id="login_form" onSubmit={this.handleSubmit}>
                 <label htmlFor="email">Email:</label>
-                <input type="email" name="email" placeholder="Napiš email" required />
+                <input type="email" name="email" placeholder="Napiš email" value={this.state.email} onChange={this.handleChange} required />
 
                 <label htmlFor="password">Heslo: </label>
                 <input
@@ -16,6 +32,8 @@ export default class LoginForm extends Component {
                     required
                     minLength="4"
                     maxLength="20"
+                    value={this.state.password}
+                    onChange={this.handleChange}
                 />
 
                 <button type="submit">Přihlásit se</button>
