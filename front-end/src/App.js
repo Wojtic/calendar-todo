eed5836071eab5d896b55b9def65613802466c85
Wojtic <62419039+Wojtic@users.noreply.github.com>
2021-04-30 22:16:54 +0200

    Added basic home path

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,18 +1,22 @@
 import Navigation from './Navigation';
 import LoginForm from './LoginForm';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
   return (
     <Router>
       <Navigation />
       <main>
-        <Route path="/login" render={(props) => (
-          <LoginForm {...props} isRegister={false} />
-        )} />
-        <Route path="/register" render={(props) => (
-          <LoginForm {...props} isRegister={true} />
-        )} />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path="/login" render={(props) => (
+            <LoginForm {...props} isRegister={false} />
+          )} />
+          <Route path="/register" render={(props) => (
+            <LoginForm {...props} isRegister={true} />
+          )} />
+        </Switch>
       </main>
     </Router >
   );
