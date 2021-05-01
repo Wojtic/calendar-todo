81bbb4f85fa7e6f4255e8880f2524cfbed1978a6
Wojtic <62419039+Wojtic@users.noreply.github.com>
2021-05-01 14:05:27 +0200

    Revert "Wrong commit"
    This reverts commit a5101df65b58859ddf9f8b0100d6891ff04c941e.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { UserProvider } from './UserContext';
 import Navigation from './Navigation';
-import RouterSwitch from './RoutesSwitch';
-import { BrowserRouter as Router } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import Home from './Home'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { UserProvider } from './UserContext';
 
 
 export default function App() {
@@ -12,10 +13,19 @@ export default function App() {
         <div className="App">
           <Navigation />
           <main>
-            <RouterSwitch />
+            <Switch>
+              <Route path='/' exact component={Home} />
+              <Route path="/login" render={(props) => (
+                <LoginForm {...props} isRegister={false} />
+              )} />
+              <Route path="/register" render={(props) => (
+                <LoginForm {...props} isRegister={true} />
+              )} />
+            </Switch>
           </main>
         </div>
       </UserProvider>
+
     </Router >
   );
 }
