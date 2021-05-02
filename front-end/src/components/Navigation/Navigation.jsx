9ad717a0ac9cb4fd4f94c27d7a5fef787a91048a
Wojtic <62419039+Wojtic@users.noreply.github.com>
2021-05-02 15:25:03 +0200

    Made navigation function component

--- front-end/src/components/Navigation/Navigation.jsx
+++ front-end/src/components/Navigation/Navigation.jsx
@@ -1,62 +1,59 @@
-import React, { Component } from "react";
+import React, { useState, useContext } from "react";
 import { UserContext } from "../contexts/UserContext.jsx";
 import { NavLink } from "react-router-dom";
 import "../../styles/css/style.css";
 
-export default class Navigation extends Component {
-  state = { ulShown: false };
-  static contextType = UserContext;
+export default function Navigation() {
+  const [ulShown, setUlShown] = useState(false);
+  const [user] = useContext(UserContext);
 
-  render() {
-    const [user] = this.context;
-    return (
-      <nav>
-        <img src="./icons/test.png" alt="<h3>Logo</h3>" height="90%" />
-        <ul className={this.state.ulShown ? "nav_active" : null}>
+  return (
+    <nav>
+      <img src="./icons/test.png" alt="<h3>Logo</h3>" height="90%" />
+      <ul className={ulShown ? "nav_active" : null}>
+        <li>
+          <NavLink
+            activeStyle={{ textShadow: "0px 0px 1px white" }}
+            to="/"
+            exact
+          >
+            Domů
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            activeStyle={{ textShadow: "0px 0px 1px white" }}
+            to="calendar"
+          >
+            Kalendář
+          </NavLink>
+        </li>
+        {user.user_name == null ? (
           <li>
             <NavLink
               activeStyle={{ textShadow: "0px 0px 1px white" }}
-              to="/"
-              exact
+              to="login"
             >
-              Domů
+              Log in
             </NavLink>
           </li>
+        ) : (
           <li>
-            <NavLink
-              activeStyle={{ textShadow: "0px 0px 1px white" }}
-              to="calendar"
-            >
-              Kalendář
-            </NavLink>
+            <i className="fa fa-user-circle fa-2x"></i>
+            <p>{user.user_name}</p>
           </li>
-          {user.user_name == null ? (
-            <li>
-              <NavLink
-                activeStyle={{ textShadow: "0px 0px 1px white" }}
-                to="login"
-              >
-                Log in
-              </NavLink>
-            </li>
-          ) : (
-            <li>
-              <i className="fa fa-user-circle fa-2x"></i>
-              <p>{user.user_name}</p>
-            </li>
-          )}
-        </ul>
-        <div
-          className={`burger ${this.state.ulShown ? "toggle" : null}`}
-          onClick={() => {
-            this.setState({ ulShown: !this.state.ulShown });
-          }}
-        >
-          <div id="burger1"></div>
-          <div id="burger2"></div>
-          <div id="burger3"></div>
-        </div>
-      </nav>
-    );
-  }
+        )}
+      </ul>
+      <div
+        className={`burger ${ulShown ? "toggle" : null}`}
+        onClick={() => {
+          setUlShown(!ulShown);
+        }}
+      >
+        <div id="burger1"></div>
+        <div id="burger2"></div>
+        <div id="burger3"></div>
+      </div>
+    </nav>
+  );
 }
