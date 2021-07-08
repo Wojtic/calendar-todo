e22ef3157c72f9ec30713de407cfc38950649603
Wojtic <62419039+Wojtic@users.noreply.github.com>
2021-07-08 14:30:37 +0200

    Changed backend to typescript

--- back-end/index.js
+++ back-end/index.js
@@ -7,172 +7,154 @@ const mysql = require("mysql");
 const bodyParser = require("body-parser");
 const app = express();
 require("dotenv").config();
-
 const jsonParser = bodyParser.json();
-
 let con = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: "calendar_todo",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "calendar_todo",
 });
-
 con.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to MySQL");
+    if (err)
+        throw err;
+    console.log("Connected to MySQL");
 });
-
 const initializePassport = require("./passport-config");
-initializePassport(
-  passport,
-  (email, callback) => {
+initializePassport(passport, (email, callback) => {
     let sql = `SELECT * FROM users WHERE email='${email}'`;
     let user;
     con.query(sql, (err, result) => {
-      if (err) throw err;
-      user = JSON.parse(JSON.stringify(result))[0];
-      return callback(null, user);
+        if (err)
+            throw err;
+        user = JSON.parse(JSON.stringify(result))[0];
+        return callback(null, user);
     });
-  },
-  (id, callback) => {
+}, (id, callback) => {
     let sql = `SELECT * FROM users WHERE id='${id}'`;
     let user;
     con.query(sql, (err, result) => {
-      if (err) throw err;
-      user = JSON.parse(JSON.stringify(result))[0];
-      return callback(null, user);
+        if (err)
+            throw err;
+        user = JSON.parse(JSON.stringify(result))[0];
+        return callback(null, user);
     });
-  }
-);
-
+});
 app.use(express.urlencoded({ extended: false }));
-
 app.listen(3080, () => console.log("Listening at 3080"));
 app.use(express.static("./src/public"));
-
 app.use(flash());
-app.use(
-  session({
+app.use(session({
     secret: process.env.PP_SECRET,
     resave: false,
     saveUninitialized: false,
-  })
-);
+}));
 app.use(passport.initialize());
 app.use(passport.session());
-
 const getGroups = (user, callback) => {
-  let query = `SELECT * FROM user_to_group WHERE user_id='${user}'`;
-  con.query(query, (err, result) => {
-    if (err) callback(err, null);
-    if (result[0]) {
-      result = JSON.parse(JSON.stringify(result));
-      callback(
-        null,
-        result.map((elem) => elem["group_name"])
-      );
-    } else {
-      callback(null, []);
-    }
-  });
-};
-
-app.post("/login", passport.authenticate("local"), (req, res) => {
-  if (req.user || req.session.user) {
-    return res.json({ user_name: req.user.username });
-  }
-  return res.status(401);
-});
-
-app.post("/register", jsonParser, (req, res) => {
-  con.query(
-    `SELECT * FROM users WHERE email='${req.body.email}'`,
-    async (err, result) => {
-      if (err) throw err;
-      if (result[0]) {
-        res.send({ userExists: true });
-      } else {
-        try {
-          const hashedPassword = await bcrypt.hash(req.body.password, 10);
-
-          const sql = `INSERT INTO users (username, email, password) VALUES ('${req.body.username}', '${req.body.email}', '${hashedPassword}')`;
-          con.query(sql, (err, result) => {
-            if (err) throw err;
-          });
-          res.send("Register succeeded");
-        } catch {
-          res.sendStatus(500);
+    let query = `SELECT * FROM user_to_group WHERE user_id='${user}'`;
+    con.query(query, (err, result) => {
+        if (err)
+            callback(err, null);
+        if (result[0]) {
+            result = JSON.parse(JSON.stringify(result));
+            callback(null, result.map((elem) => elem["group_name"]));
         }
-      }
+        else {
+            callback(null, []);
+        }
+    });
+};
+app.post("/login", passport.authenticate("local"), (req, res) => {
+    if (req.user || req.session.user) {
+        return res.json({ user_name: req.user.username });
     }
-  );
+    return res.status(401);
 });
-
+app.post("/register", jsonParser, (req, res) => {
+    con.query(`SELECT * FROM users WHERE email='${req.body.email}'`, async (err, result) => {
+        if (err)
+            throw err;
+        if (result[0]) {
+            res.send({ userExists: true });
+        }
+        else {
+            try {
+                const hashedPassword = await bcrypt.hash(req.body.password, 10);
+                const sql = `INSERT INTO users (username, email, password) VALUES ('${req.body.username}', '${req.body.email}', '${hashedPassword}')`;
+                con.query(sql, (err, result) => {
+                    if (err)
+                        throw err;
+                });
+                res.send("Register succeeded");
+            }
+            catch {
+                res.sendStatus(500);
+            }
+        }
+    });
+});
 app.get("/log_out", (req, res) => {
-  req.logOut();
-  res.redirect("/");
-});
-
-app.get("/delete_account", (req, res) => {
-  con.query(`DELETE FROM users WHERE id='${req.user.id}'`, (err, response) => {
-    if (err) throw err;
     req.logOut();
     res.redirect("/");
-  });
 });
-
+app.get("/delete_account", (req, res) => {
+    con.query(`DELETE FROM users WHERE id='${req.user.id}'`, (err, response) => {
+        if (err)
+            throw err;
+        req.logOut();
+        res.redirect("/");
+    });
+});
 app.post("/create_task", jsonParser, (req, res) => {
-  if (!(req.body.name && req.body.date && req.body.owner)) {
-    res.sendStatus(400);
-  } else if (req.user || req.session.user) {
-    let queryString = `INSERT INTO tasks (task_name, task_description, task_date) VALUES ('${
-      req.body.name
-    }', '${req.body.description || ""}', '${req.body.date}')`;
-
-    con.query(queryString, (err, response) => {
-      if (err) throw err;
-      queryString = "SELECT LAST_INSERT_ID()";
-      con.query(queryString, (err, response) => {
-        if (err) throw err;
-        task_id = JSON.parse(JSON.stringify(response[0]));
-        task_id = task_id[Object.keys(task_id)[0]];
-        queryString = `INSERT INTO task_to_owner (task_id, ${
-          req.body.owner.substring(0, 6) == "group-" ? "group_name" : "user_id"
-        }) VALUES (${task_id}, ${
-          req.body.owner.substring(0, 6) == "group-"
-            ? `'${req.body.owner}'`
-            : req.user.id
-        })`;
+    if (!(req.body.name && req.body.date && req.body.owner)) {
+        res.sendStatus(400);
+    }
+    else if (req.user || req.session.user) {
+        let queryString = `INSERT INTO tasks (task_name, task_description, task_date) VALUES ('${req.body.name}', '${req.body.description || ""}', '${req.body.date}')`;
         con.query(queryString, (err, response) => {
-          if (err) throw err;
-          else res.sendStatus(200);
+            if (err)
+                throw err;
+            queryString = "SELECT LAST_INSERT_ID()";
+            con.query(queryString, (err, response) => {
+                if (err)
+                    throw err;
+                let task_id = JSON.parse(JSON.stringify(response[0]));
+                task_id = task_id[Object.keys(task_id)[0]];
+                queryString = `INSERT INTO task_to_owner (task_id, ${req.body.owner.substring(0, 6) == "group-" ? "group_name" : "user_id"}) VALUES (${task_id}, ${req.body.owner.substring(0, 6) == "group-"
+                    ? `'${req.body.owner}'`
+                    : req.user.id})`;
+                con.query(queryString, (err, response) => {
+                    if (err)
+                        throw err;
+                    else
+                        res.sendStatus(200);
+                });
+            });
         });
-      });
-    });
-  } else {
-    res.sendStatus(401);
-  }
+    }
+    else {
+        res.sendStatus(401);
+    }
 });
-
 app.get("/get_tasks", (req, res) => {
-  if (req.user || req.session.user) {
-    // select all tasks with user id
-    let query = `SELECT * FROM task_to_owner WHERE user_id=${req.user.id}`;
-    con.query(query, (err, result) => {
-      if (err) throw err;
-      console.log(result);
-    });
-    // select all tasks with groups the user is part of
-  } else {
-    res.sendStatus(401);
-  }
+    if (req.user || req.session.user) {
+        let query = `SELECT * FROM task_to_owner WHERE user_id=${req.user.id}`;
+        con.query(query, (err, result) => {
+            if (err)
+                throw err;
+            console.log(result);
+        });
+    }
+    else {
+        res.sendStatus(401);
+    }
 });
-
 app.get("/get_groups", (req, res) => {
-  if (req.user.id || req.session.user.id) {
-    getGroups(req.user.id, (err, result) => {
-      if (err) throw err;
-      res.send(result);
-    });
-  }
+    if (req.user.id || req.session.user.id) {
+        getGroups(req.user.id, (err, result) => {
+            if (err)
+                throw err;
+            res.send(result);
+        });
+    }
 });
--- back-end/passport-config.js
+++ back-end/passport-config.js
@@ -1,34 +1,33 @@
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcrypt');
-
+const LocalStrategy = require("passport-local").Strategy;
 function initialize(passport, getUserByEmail, getUserById) {
     const authenticateUser = async (email, password, done) => {
         getUserByEmail(email, async (err, user) => {
-            if (err) throw err;
+            if (err)
+                throw err;
             if (user == null) {
-                return done(null, false, { message: 'No user with that email' });
+                return done(null, false, { message: "No user with that email" });
             }
-
             try {
                 if (await bcrypt.compare(password, user.password)) {
                     return done(null, user);
-                } else {
-                    return done(null, false, { message: 'Password incorrect' });
                 }
-            } catch (e) {
+                else {
+                    return done(null, false, { message: "Password incorrect" });
+                }
+            }
+            catch (e) {
                 return done(e);
             }
         });
-
     };
-    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+    passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
     passport.serializeUser((user, done) => done(null, user.id));
     passport.deserializeUser((id, done) => {
         getUserById(id, (err, user) => {
-            if (err) throw err;
+            if (err)
+                throw err;
             done(null, user);
-        })
+        });
     });
 }
-
 module.exports = initialize;
