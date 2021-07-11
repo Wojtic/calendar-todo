5f800d239fe51f8a8dbb185539a6bb37b85618ab
Wojtic <62419039+Wojtic@users.noreply.github.com>
2021-07-11 18:53:20 +0200

    Gitignored .js files

--- front-end/src/components/contexts/UserContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-export const UserNameContext = createContext({
-    userName: null,
-    setUserName: (name) => console.warn("No user name provided"),
-});
--- front-end/src/index.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-ReactDOM.render(_jsx(React.StrictMode, { children: _jsx(App, {}, void 0) }, void 0), document.getElementById("root"));
